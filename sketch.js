d669547b8314de00e0e017551199e0eb2cdc437e
Julia Nascimento <ju.araujo11@yahoo.com.br>
2022-02-03 19:11:58 -0300

    Create ground

--- sketch.js
+++ sketch.js
@@ -1,17 +1,37 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+const Constraint = Matter.Constraint
+
+var motor, mundo
+var chao
+var fundoImagem
 
 function preload() {
-
+  fundoImagem = loadImage('assets/background.gif')
 }
 
 function setup() {
+  createCanvas(1200, 600)
 
+  motor = Engine.create()
+  mundo = motor.world
+
+  rectMode(CENTER)
+
+  var opcoes = {
+    isStatic: true
+  }
+
+  chao = Bodies.rectangle(600, 598, 1200, 4, opcoes)
+  World.add(mundo, chao)
 }
 
 function draw() {
-  background('black');
+  image(fundoImagem, 0, 0, 1200, 600)
+
+  Engine.update(motor)
+
+  rect(chao.position.x, chao.position.y, 1200, 4)
   
 }
